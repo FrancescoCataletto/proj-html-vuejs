2c21269de65ead563ff9147157e22b224d252389
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-05-27 15:27:34 +0200

    header draft: div for logo, draft menu, draft searchbar

--- src/App.vue
+++ src/App.vue
@@ -24,5 +24,10 @@ export default {
 </script>
 
 <style lang="scss">
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 
 </style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,24 @@
 <template>
-  <header>
-      H
+  <header class="d-flex justify-content-between align-items-center">
+      <div id="left-header">
+          <img src="" alt="MaxCoach Logo">
+      </div>
+      <nav>
+          <ul>
+              <li>Home</li>
+              <li>Pages</li>
+              <li>Courses</li>
+              <li>Features</li>
+              <li>Blog</li>
+              <li>Shop</li>
+          </ul>
+      </nav>
+      <div id="right-header" class="d-flex">
+          <div>
+              <img src="../assets/img/en.png" alt="UK flag">
+          </div>
+          <input type="text" class="form-control">
+      </div>
   </header>
 </template>
 
@@ -11,5 +29,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/img/global.scss";
 
+header{
+    height: 80px;
+    background-color: bisque;
+    width: 100vw;
+}
+
+nav{
+    width: 40%;
+    ul{
+        display: flex;
+        width: 100%;
+        justify-content: space-around;
+        li{
+            list-style: none;
+        }
+    }
+}
 </style>
