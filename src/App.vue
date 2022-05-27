904c472767ed664ffbe00490bd53e65f1e2d41f1
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-05-27 14:47:02 +0200

    draft structure: header, jumbo, main, form, footer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
-    
+
+    <HeaderComponent />
+    <JumbotronComponent />
+    <MainComponent />
+    <FormComponent />
+    <FooterComponent />
+
   </div>
 </template>
 
 <script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import JumbotronComponent from './components/JumbotronComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+import FormComponent from './components/FormComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
 
 export default {
-  name: 'App'
+    name: "App",
+    components: { HeaderComponent, JumbotronComponent, MainComponent, FormComponent, FooterComponent }
 }
 </script>
 
---
+++ src/components/JumbotronComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      J
+  </div>
+</template>
+
+<script>
+export default {
+    name: "JumbotronComponent"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
