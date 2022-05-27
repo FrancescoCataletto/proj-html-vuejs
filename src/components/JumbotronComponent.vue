ad50d49c1b4508c3ef1ec43a6f6a16a2f442c114
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-05-27 15:50:40 +0200

    jumbotron draft without element in position absolute

--- src/components/JumbotronComponent.vue
+++ src/components/JumbotronComponent.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-      J
+  <div id="jumbotron">
+        <div id="left-jumbo">
+          <h3>
+            New Challenges, New Skills
+          </h3>
+          <h1>
+            BUILD YOUR OWN LIFE COACHING BUSINESS
+          </h1>
+          <h5>
+            Whole-life Business Coaching for committed entrepreneurs
+          </h5>
+          <button>Get started today</button>
+        </div>
+        <div id="right-jumbo">
+          <img src="../assets/img/home-business-hero-avatar.png" alt="">
+        </div>
   </div>
 </template>
 
@@ -11,5 +25,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#jumbotron{
+    height: 724px;
+    background-color: rgb(239, 242, 246);
+}
 
+#left-jumbo{
+  width: 50%;
+  height: 100%;
+  float: left;
+}
+
+#right-jumbo{
+  width: 50%;
+  background-image: url("../assets/img/home-business-hero-global-image.png");
+  background-size: contain;
+  background-repeat: no-repeat;
+  float: right;
+}
 </style>
