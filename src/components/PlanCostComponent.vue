8623f09c42129fb2ba81ada470e9a378f149961d
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-05-30 20:09:55 +0200

    plancost component structure completed: need to add shadow at bottom of cards

--- src/components/PlanCostComponent.vue
+++ src/components/PlanCostComponent.vue
@@ -15,27 +15,27 @@
           <div class="plan-card">
               <img src="../assets/img/course-02-443x600.jpg" alt="">
               <div>
-                  <p>$40.00</p>
-                  <p>Learning to Write as  Professional Author</p>
+                  <p>Free</p>
+                  <p>Customer-centric Info-Tech Strategies</p>
               </div>
           </div>
           <div class="plan-card">
               <img src="../assets/img/course-02-443x600.jpg" alt="">
               <div>
-                  <p>$40.00</p>
-                  <p>Learning to Write as  Professional Author</p>
+                  <p>$19.00</p>
+                  <p>Open Programming Courses for Everyone: Python</p>
               </div>
           </div>
           <div class="plan-card">
               <img src="../assets/img/course-02-443x600.jpg" alt="">
               <div>
-                  <p>$40.00</p>
-                  <p>Learning to Write as  Professional Author</p>
+                  <p>$26.00</p>
+                  <p>Academic Listening and Note-taking</p>
               </div>
           </div>
       </div>
       <div id="plan-cost-bottom">
-          <p>Take the control of their life back and start doing things to make their dream come true. <span><a href="#">View all courses</a></span></p>
+          <p>Take the control of their life back and start doing things to make their dream come true. <span><a href="#">View all courses<i class="fa-solid fa-arrow-right"></i></a></span></p>
       </div>
   </div>
 </template>
@@ -55,8 +55,13 @@ export default {
 
 #plan-cost-top{
     text-align: center;
+    margin: 50px 0;
     h3{
-        margin-bottom: 30px;
+        color: $brand-color;
+        font-size: 18px;
+    }
+    h2{
+        font-weight: bold;
     }
 }
 
@@ -71,17 +76,36 @@ export default {
     position: relative;
     img{
         width: 100%;
+        border-radius: 5px;
     }
     div{
         position: absolute;
         bottom: 0px;
         left: 50px;
+        width: 70%;
+        p:first-child{
+            color: $brand-color;
+            font-weight: bold;
+        }
+        p:nth-child(2){
+            color: white;
+            font-weight: bold;
+        }
     }
 }
 
 #plan-cost-bottom{
-    width: 50%;
-    margin: 30px auto;
+    width: 30%;
+    margin: 50px auto;
     text-align: center;
+    font-size: 14px;
+    a{
+        color: rgb(50, 48, 67);
+        font-size: 14px;
+        i{
+            font-size: 12px;
+            margin-left: 3px;
+        }
+    }
 }
 </style>
